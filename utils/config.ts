bb8d8cecb389138f755d9d287ae0ee9d2cacad11
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-11-16 09:09:16 +0200

    basic upload functionality

--- utils/config.ts
+++ utils/config.ts
@@ -3,4 +3,7 @@
 
 import { PinataSDK } from "pinata-web3"
 
-export 
+export const pinata = new PinataSDK({
+    pinataJwt: `${process.env.PINATA_JWT}`,
+    pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`
+})
